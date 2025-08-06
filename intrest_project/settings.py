369d98949f32100a3e78b333f2d8ad13974c1ad2
parasuram <parasu7824@gmail.com>
2025-08-06 09:33:52 +0530

    Update settings.py

--- intrest_project/settings.py
+++ intrest_project/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'mokk0nwua@oqtged*fa+skcuyi)+_)di_h*2+&h##i0**4k6g4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['calculator-k7h8.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
